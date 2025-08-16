2fb599d9c2820d18bf138e371476f84df9c516e7
Pranjal Garg <pranjalgarg2005@gmail.com>
2025-08-16 20:34:39 +0530

    fix: java version

---
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -0,0 +1,40 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Product;
+import com.example.demo.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    ProductService service;
+
+    @GetMapping("/products")
+    public List<Product> getProducts() {
+        return service.getProducts();
+    }
+
+    @GetMapping("/products/{prodId}")
+    public Product getProductById(@PathVariable int prodId) {
+        return service.getProductById(prodId);
+    }
+
+    @PostMapping("/products")
+    public void addProduct(@RequestBody Product product) {
+        service.addProduct(product);
+    }
+
+    @PutMapping("/products")
+    public void updateProduct(@RequestBody Product product) {
+        service.updateProduct(product);
+    }
+
+    @DeleteMapping("/products/{prodId}")
+    public void deleteProductById(@PathVariable int prodId) {
+        service.deleteProduct(prodId);
+    }
+}
